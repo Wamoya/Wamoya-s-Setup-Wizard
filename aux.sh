763df6abb5aca54277fb8f347ad5d4e0a3ed9fc3
Wamoya <juangrossodll@gmail.com>
2025-02-20 19:00:23 +0100

    Add more personal preferences

--- aux.sh
+++ aux.sh
@@ -9,8 +9,8 @@ sudo snap install nvim --classic
 mkdir ~/downloads ~/repos
 
 # Load config files
-cp ./configs/.bash_aliases ~/.bash_aliases
-source ~/.bash_aliases
+cp ./configs/bash_aliases ~/.bash_aliases
+# source ~/.bash_aliases # TODO: Aliases are not loaded right since this command is executed in a subshell! I have to find a way to fix it.
 
 # Clone repos
 git clone https://github.com/Wamoya/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
